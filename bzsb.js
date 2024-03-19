0c09ffc208a760e626b3e9d0f52a444a82c0d93e
KUAI <2542862169@qq.com>
2024-03-19 20:45:35 +0800

    Update bzsb.js

--- bzsb.js
+++ bzsb.js
@@ -2,10 +2,16 @@
   const src = document.querySelector('iframe').src;
   if (!src) console.error('iframe检测异常，请在top层级运行');
   const url = new URL(src);
-  const fileName = url.searchParams.get('a100');
+  let fileName = url.searchParams.get('a100');
   if (!fileName) console.error('未获取到文件名');
-  let save_link = document.createElement('a');
-  save_link.href = "https://www.bzsb.info/searchStandard.do?action=getPdf";
-  save_link.download = fileName + '.pdf';
+  const save_link = document.createElement('a');
+  save_link.href = 'https://www.bzsb.info/searchStandard.do?action=getPdf';
+  fileName = fileName.replaceAll(' ', '_') + '.pdf';
+  fileName = prompt('请输入文件名，按取消则不下载', fileName);
+  if (!fileName) {
+    console.error('未获取到文件名');
+    return;
+  }
+  save_link.download = fileName;
   save_link.click();
 })();
